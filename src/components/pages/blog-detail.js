d6aeaa6b4ab89d925e5bfdd65d0e38b7ff89c44d
mikecarouchi <mikecarouchi@gmail.com>
2021-10-27 11:41:06 -0700

    Added initial blog features

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class BLogDetail extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            currentId
+        }
+    }
+    render() {
+        return (
+            <div>
+                <h1>Blog Details</h1>
+            </div>
+        );
+    }
+}
